f5ac6f274b111d1e4405a9a37f696841864e589c
Gordon Yang <rungless@outlook.com>
2015-11-25 22:56:07 +1300

    Published to azure, api working

---
+++ contoso/contoso/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace contoso.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Points { get; set; }
+        public string Assignments { get; set; }
+        public string Tests { get; set; }
+
+    }
+}
---
+++ contoso/contoso/Models/contosoContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace contoso.Models
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+
+    public class contosoContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public class MyConfiguration : DbMigrationsConfiguration<contosoContext>
+        {
+            public MyConfiguration()
+            {
+                this.AutomaticMigrationsEnabled = true;
+            }
+
+            protected override void Seed(contosoContext context)
+            {
+
+                var courses = new List<Course>
+            {
+                new Course {CourseID = 1050, Title = "Compsci 101",      Points = 15, Assignments = "3 assignments", Tests ="1 test" },
+                new Course {CourseID = 4022, Title = "Compsci 105", Points = 15, Assignments = "3 assignments", Tests ="1 test" },
+                new Course {CourseID = 4041, Title = "Infosys 322", Points = 15, Assignments = "3 assignments", Tests ="1 test" },
+                new Course {CourseID = 1045, Title = "Infosys 321",       Points = 15, Assignments = "no assignments", Tests ="2 tests" },
+
+            };
+                courses.ForEach(s => context.Courses.AddOrUpdate(p => p.Title, s));
+                context.SaveChanges();
+            }
+
+        }
+
+        public contosoContext() : base("name=contosoContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<contosoContext, MyConfiguration>());
+
+        }
+
+        public System.Data.Entity.DbSet<contoso.Models.Course> Courses { get; set; }
+    }
+}
