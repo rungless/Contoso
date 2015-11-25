5a7dc40dc59362d765926e7eae984dd114ddd9d6
Gordon Yang <rungless@outlook.com>
2015-11-26 00:08:28 +1300

    courses api wokring correctly.

---
+++ contoso/contoso/Scripts/courseModule.js
@@ -0,0 +1,17 @@
+﻿
+
+var courseModule = (function () {
+    return {
+        getCourses: function (callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://conweb.azurewebsites.net/api/courses",
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        }
+
+    };
+}());
---
+++ contoso/contoso/Scripts/index.js
@@ -0,0 +1,46 @@
+﻿
+    // This event triggers on page load
+
+    document.addEventListener("DOMContentLoaded", function () {
+        loadCourses();
+    });
+
+    function loadCourses() {
+
+        courseModule.getCourses(setupCourseTable);
+
+    }
+
+    // This is the callback for when the data comes back in the studentmodule
+    function setupCourseTable(courseList) {
+
+        // We need a reference to the div/table that we are going to chuck our data into
+        var coursesTable = document.getElementById("courseList");
+
+        console.log(courseList);
+
+        for (i = 0; i < courseList.length; i++) {
+            // create row
+            var row = document.createElement('tr');
+
+            // add the columns in the row (td /data cells)
+            var courseNameCol = document.createElement('td');
+            courseNameCol.innerHTML = courseList[i].Title;
+            row.appendChild(courseNameCol);
+
+            var pointsCol = document.createElement('td');
+            pointsCol.innerHTML = courseList[i].Points;
+            row.appendChild(pointsCol);
+
+            var assignmentsCol = document.createElement('td');
+            assignmentsCol.innerHTML = courseList[i].Assignments;
+            row.appendChild(assignmentsCol);
+
+            var testsCol = document.createElement('td');
+            testsCol.innerHTML = courseList[i].Tests;
+            row.appendChild(testsCol);
+
+            //Append the row to the end of the table
+            coursesTable.appendChild(row);
+        }
+    }
